09909fc667b1073b83f0d8daebbaab95b3ad0b74
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2021-12-27 14:03:52 +0300

    tour

--- php/tournament.php
+++ php/tournament.php
@@ -22,55 +22,35 @@ public function createPairs(){
   foreach($this->players as $k=>$v){
     $allplayers[]=$v[array_key_first($v)];
   }
- //все возможные пары игроков 
-  $pairs=[];
-  $size=count($allplayers)-1;
-  $keys = array_keys($allplayers);
-  $i=0;
-        foreach($allplayers as $player){
-        
-            for($j=$size;$j>$i;$j--)
-            {   
-                $pairs[$keys[$i].$keys[$j]]=
-                  $allplayers[$i]."-".
-                  $allplayers[$j];
-        
-               
-            }  
-            
-    $i++;
-        
-        }
 
-    ksort($pairs);
-    $pairs=array_values($pairs);
-  //  турнир
-
-
-    $games=[];
-
- 
-      for($j=0,$i=count($pairs)-1;$j<$i;$j++,$i--){
- 
-          $games[]=[
-           'name'=> $this->name,
-           'date'=>date("d.m.Y", strtotime(date("d.m.Y",strtotime(implode(".",(array_reverse(explode('.',$this->date))))))."+$j days")),
-            $pairs[$i],
-            $pairs[$j],
-      
-      
-         ];
-        
-        
+  $n = count($allplayers);
+for ($r = 0; $r < $n - 1; $r++) { 
+  $rounds[$r]=[$this->name,date("d.m.Y", strtotime(date("d.m.Y",strtotime(implode(".",(array_reverse(explode('.',$this->date))))))."$r days"))];
+   for ($i = 0; $i < $n / 2; $i++) {
+      if($i==$n - 1 - $i){
+         continue;
       }
+
+      $rounds[$r][] = $allplayers[$i]. $allplayers[$n - 1 - $i];
+   }
+
+   $allplayers[] = array_splice($allplayers, 1, 1)[0];
+}
+   $allplayers[] = array_splice($allplayers, 1, 1)[0];
+
+  
+ 
     $str="";
-       foreach($games as $k=>$v){
-       
-         foreach($v as $key=>$value){
-             $str.=$value."\n";
-         }
+   
+     foreach($rounds as $k){
+       foreach ($k as $key => $value) {
+          $str.=$value."\n";
        }
-       return $str;
+       $str.="<br>";
+     }
+    
+ 
+       echo $str;
    }
 
 }
